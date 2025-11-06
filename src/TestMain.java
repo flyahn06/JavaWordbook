67a5a7068005b61d936533a87d13d71e558aa972
flyahn06 <flyahn06@gmail.com>
2025-11-06 18:38:26 +0900

    안녕테스트

--- src/TestMain.java
+++ src/TestMain.java
@@ -4,5 +4,7 @@ public class TestMain {
         System.out.println("hello");
         System.out.println("안녕하세요!");
         System.out.println("안녕~");
+
+        System.out.println("안녕안녕");
     }
 }
